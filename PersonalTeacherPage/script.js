a830890d43125fec51a7ea733d67d6764f504232
Aleksey Kabanov <31734549+Yasssnee@users.noreply.github.com>
2022-11-15 14:50:59 +0200

    Add files via upload

---
+++ PersonalTeacherPage/script.js
@@ -0,0 +1,63 @@
+let result = confirm("Hi. Lets me tell you a little bit about me");
+
+if (result == true) {
+alert("My name is Gosling Zakhar Viktorovych");
+} else {
+end();
+}
+
+result = confirm("Press OK to read more about me");
+
+if (result == true) {
+alert("Personal page of a physical education teacher");
+} else {
+alert("Gosling Zakhar Viktorovych");
+}
+
+result = confirm("Next");
+
+if (result == true) {
+alert("Zakhar Viktorovych was born on November 12, 1980. He studied at the Mogilian Academy in Kyiv, and at the same time he was involved in many sports");
+} else {
+alert("Personal page of a physical education teacher");
+}
+
+result = confirm("Next");
+
+if (result == true) {
+alert("So, before graduating from the academy, he already had a black belt in judo, and a dozen invitations to various NBA teams, but as a football fan, he rejected them all, responding only to the Lakers with his autograph, which can still be seen on the honor board at their headquarters apartment in LA");
+} else {
+alert("Zakhar Viktorovych was born on November 12, 1980. He studied at the Mogilian Academy in Kyiv, while doing many sports.");
+}
+
+result = confirm("Next");
+
+if (result == true) {
+alert("Master of sports Zakhar Viktorovych can professionally teach both physical education and other disciplines related to sports: football, basketball, volleyball, baseball, boxing, juto, karate, athletics and much more");
+} else {
+alert("So, before graduating from the academy, he already had a black belt in judo, and a dozen invitations to various NBA teams, but as a football fan, he rejected them all, responding only to the Lakers with his autograph, which can still be seen on the honor board at their headquarters apartment in LA");
+}
+
+result = confirm("Next");
+
+if (result == true) {
+alert("You can learn more by phone +(380)38-380-38-38, or by mail: zahar.is.fckng.machine@gmail.com");
+} else {
+alert("Master of sports Zakhar Viktorovych can professionally teach both physical education and other disciplines related to sports: football, basketball, volleyball, baseball, boxing, juto, karate, athletics and much more");
+}
+
+result = confirm("Next");
+
+if (result == true) {
+alert("©Goszahar. All rights reserved");
+} else {
+alert("You can learn more by phone +(380)38-380-38-38, or by mail: zahar.is.fckng.machine@gmail.com");
+}
+
+result = confirm("Go to page?");
+
+if (result == true) {
+end();
+} else {
+alert("©Goszahar. All rights reserved");
+}
